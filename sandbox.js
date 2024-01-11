3e1a9e6cd464dff48b4f0367d7d46904971773a7
jerzyk <ryanjerzyk@gmail.com>
2024-01-11 08:13:07 -0500

    we added the bolt ID and some stuff with bolt

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,13 @@
 //Programer:Ryan the Jerzyk
 //Date: 1.10.2024
 //Program: Sphero Bolt Testing
+//Bolt ID: CF6D
+
+async functions startProgram() {
+	await speak("Hello World", true);
+	setMainLed({r:0, g: 0, b: 255 });
+	setSpeed(60);
+	await delay (2);
+	setSpeed(0);
+	
+}
