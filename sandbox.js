e1ce8f7ccaedb64a8ee7b602f577f73f74948bb7
jerzyk <ryanjerzyk@gmail.com>
2024-01-18 08:39:13 -0500

    1.18.24

--- sandbox.js
+++ sandbox.js
@@ -3,18 +3,24 @@
 //Program: Sphero Bolt Testing
 //Bolt ID: CF6D
 
-/*
-async functions startProgram() {
+async function startProgram() {
 	await speak("Hello World", true);
 	setMainLed({r:0, g: 0, b: 255 });
 	setSpeed(60);
 	await delay (2);
 	setSpeed(0);
 }
-*/
 
 
 //Square w/ diffrent colors for each line
+async function startprogram(){
+for (var _i1 = 0; _i1 < 4; _i1++) 
+	setMainLed(getRandomColor())
+	await delay(1)
+	await spin (90, 2)
+	
+
+}
 
 
 
@@ -27,4 +33,4 @@ async functions startProgram() {
 
 
 
-//Figure 8
+//Figure 8 
