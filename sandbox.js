5876301047d539b3e28e81732d5497cebca2f8aa
jerzyk <ryanjerzyk@gmail.com>
2024-01-19 08:43:06 -0500

    1.19.24

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,9 @@
-//Programer:Ryan the Jerzyk
+//Programer:Tate Keck
 //Date: 1.10.2024
 //Program: Sphero Bolt Testing
 //Bolt ID: CF6D
 
+/*
 async function startProgram() {
 	await speak("Hello World", true);
 	setMainLed({r:0, g: 0, b: 255 });
@@ -10,14 +11,12 @@ async function startProgram() {
 	await delay (2);
 	setSpeed(0);
 }
-
+*/
 
 //Square w/ diffrent colors for each line
-async function startprogram(){
-for (var _i1 = 0; _i1 < 4; _i1++) 
-	setMainLed(getRandomColor())
-	await delay(1)
-	await spin (90, 2)
+async function startProgram(){
+	setMainLed(getRandomColor());
+	await roll(90, 50, 1);
 	
 
 }
