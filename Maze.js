48dc200a54843139954b327a7874ba7da86bd4ee
jerzyk <ryanjerzyk@gmail.com>
2024-01-22 08:43:47 -0500

    made it say start and write start

--- Maze.js
+++ Maze.js
@@ -2,3 +2,9 @@
 // date:1.22.24
 // program: Maze
 //bolt id:CF6D
+
+//start message
+async function startProgram() {
+	await speak("START", true);
+	await scrollMatrixText('start', {r: 50, g: 50, b: 50}, 30, true)
+}
