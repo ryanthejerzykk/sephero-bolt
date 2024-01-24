2a08acfc081d9c3492c9df74f8c82293113c6a56
jerzyk <ryanjerzyk@gmail.com>
2024-01-24 08:14:12 -0500

    did the blue checkpoint

--- Maze.js
+++ Maze.js
@@ -1,10 +1,16 @@
-// members; Ryan Jerzyk, Tate Keck
-// date:1.22.24
-// program: Maze
-//bolt id:CF6D
-
-//start message
+//start
 async function startProgram() {
 	await speak("START", true);
 	await scrollMatrixText('start', {r: 50, g: 50, b: 50}, 30, true)
+
+	
+//blue checpoint
+	await delay(1)
+	await roll(0,130,1)
+	await delay (1)
+	await setMainLed({r:0,g:0,b:255})
 }
+
+
+//blue checkpoint
+
