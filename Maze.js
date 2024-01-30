c4ead9340ea3c4309e4083ca3aa03175aaf964f3
jerzyk <ryanjerzyk@gmail.com>
2024-01-30 08:42:22 -0500

    1/30/24

--- Maze.js
+++ Maze.js
@@ -6,31 +6,31 @@ async function startProgram() {
 	
 //blue checpoint
 	await delay(1)
-	await roll(0,140,1)
+	await roll(0,145,1)
 	await delay (1)
 	await setMainLed({r:0,g:0,b:255})
 // first sound
 	await delay (1)
-	await roll(90,50,2)
+	await roll(90,52,2)
 	await Sound.Effects.Explosion.play(true)
 //red checkpoint
-	await roll(180,44,1.5)
+	await roll(180,50,1.5)
 	await delay(1)
-	await roll(220,44,1.5)
+	await roll(220,47,1.5)
 	await setMainLed({r:225,g:0,b:0})
 //sound two
 	await delay(1)
-	await roll(850,42,1.5)
+	await roll(850,25,3)
 	await delay(1)
-	await roll(400,40,1.5)
+	await roll(400,42,1.5)
 	await Sound.Animal.Eagle.play(true)
 // green checkpoint
 	await delay(1)
 	await roll (140,40,1.5)
 	await delay(1)
-	await roll (180,40,1.5)
+	await roll (180,25,1.5)
 	await delay(1)
-	await roll (90,50,1)
+	await roll (90,55,1)
 	await setMainLed({r:0,g:255,b:0})
 	
 }
